ca86308f7b4affc9eb6276211dbb689a2cca461a
Lugaricci <tomas.partl.jr@email.cz>
2024-03-24 08:50:31 +0100

    nyni funguje psani zprav

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -2,4 +2,15 @@ class ChatroomController < ApplicationController
   def index
     @messages = Message.includes(:user).last(50)
   end
+
+  def create_message
+    @message = Message.new(message_params)
+    @message.user_id = 1
+    @message.save
+  end
+
+  def message_params
+  params.require(:message).permit(:content)
+  end
+
 end
