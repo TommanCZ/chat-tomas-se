5774257bdee866c41e423237a3061ad088ed1903
Lugaricci <tomas.partl.jr@email.cz>
2024-03-28 18:28:39 +0100

    You can create users now but feature for confirming password is still missing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
+require 'securerandom'
+
 class UsersController < ApplicationController
   def index
   end
 
   def create
+    @user = User.new(user_params)
+    @user.save
   end
 
   def new
+    @user = User.new()
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :passhash)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
   has_many :messages
+  before_save :hash_passwords
+
+  validates :username, presence: true, uniqueness: true
+  validates :passhash, presence: true
+
+  def hash_passwords
+    self.salt = SecureRandom.hex(32)
+    self.passhash = Digest::SHA256.hexdigest("#{passhash}#{salt}")
+  end
 end
