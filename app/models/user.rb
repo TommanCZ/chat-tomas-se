cb4bf282ce604545b8ff3260f03198230d9a56bd
Lugaricci <tomas.partl.jr@email.cz>
2024-06-06 17:17:15 +0200

    minor changes in main page html file

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,4 @@ class User < ApplicationRecord
   include Clearance::User
 
   has_many :messages
-  before_save :hash_passwords
-
-  validates :username, presence: true, uniqueness: true
-  validates :passhash, presence: true
-
-  def hash_passwords
-    self.salt = SecureRandom.hex(32)
-    self.passhash = Digest::SHA256.hexdigest("#{passhash}#{salt}")
-  end
 end
